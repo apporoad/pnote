a3bb1023bf99eb633b6f891eedb624861a1f0bb6
apporoad <apporoadina@163.com>
2016-10-08 15:25:53 +0800

    add

--- site/routes/pnote.js
+++ site/routes/pnote.js
@@ -1,22 +1,20 @@
 var express = require('express');
-require('NString');
 var router = express.Router();
-
+router.get('/save',function(req,res,next){
+   //获取path
+    var path ='abc';//req.body.path;
+   //获取txt
+    //var txt=req.body.txtMain;
+   res.send(path);
+});
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/*', function(req, res, next) {
     //获取orginPath
     var path = req.originalUrl;
-    path=path.trimEnd('/');
     //获取文件内容
     var txt= 'test';
   res.render('pnote/index', { text: txt,path: path });
 });
 
-router.get('/save',function(req,res,next){
-   //获取path
-    var path =req.body.path;
-   //获取txt
-    var txt=req.body.txtMain;
-   res.render('pnote/index', {text:txt,path:path});
-});
+
 module.exports = router;
