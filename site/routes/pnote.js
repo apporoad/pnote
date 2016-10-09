d4de4db92a052c5d63dcc3188248c36ea24c885b
apporoad <apporoadina@163.com>
2016-10-09 20:43:09 +0800

    add some

--- site/routes/pnote.js
+++ site/routes/pnote.js
@@ -1,19 +1,45 @@
 var express = require('express');
 var router = express.Router();
-router.get('/save',function(req,res,next){
+var db=require('./js/mongoHelper');
+router.post('/save',function(req,res){
    //获取path
     var path =req.body.path;
-   //获取txt
-    //var txt=req.body.txtMain;
-   res.send(path);
+    var text = req.body.text;
+
+    //修改数据库
+    var p3={};
+    p3.collection="remark";
+    p3.updateStr={$set: { "text" : text }};
+    p3.whereStr={"path":path};
+    db.update(null,p3,function(result){
+    });
+    res.json({success:path});
 });
 /* GET home page. */
 router.get('/*', function(req, res, next) {
     //获取orginPath
     var path = req.originalUrl;
     //获取文件内容
-    var txt= 'test';
-  res.render('pnote/index', { text: txt,path: path });
+    var p2={};
+    p2.collection= 'remark';
+    p2.whereStr={"path":path};
+    db.select(null,p2,function (result) {
+        var txt = '';
+        if(result.length >0)
+        {
+            txt=result[0].text;
+        }
+        else
+        {
+            //插入一条空数据
+            var p1={};
+            p1.collection='remark';
+            p1.data= {"path":path,"text":""};
+            db.insert(null,p1,function (result) {});
+        }
+        res.render('pnote/index', { text: txt,path: path });
+    });
+
 });
 
 
