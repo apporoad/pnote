f4facf29424e69468e608952559a6850a1ed2e6e
apporoad <apporoadina@163.com>
2016-10-09 16:51:11 +0800

    add all

--- site/Demos/dbTest.js
+++ site/Demos/dbTest.js
@@ -1,15 +1,101 @@
-var mongoose = require('mongoose');    //引用mongoose模块
-mongoose.connect("mongodb://localhost:27017/test");
-var db = mongoose.connection;
+var MongoClient = require('mongodb').MongoClient;
+var DB_CONN_STR = 'mongodb://localhost:27017/test';
 
 
-var PNoteSchema = new mongoose.Schema({
-      path:String,   //定义一个属性name，类型为String
-      text:String
+//插入
+var insertData = function(db, callback) {
+    //连接到表
+    var collection = db.collection('tb2');
+    //插入数据
+    var data = [{"name":'wilson001',"age":21},{"name":'wilson002',"age":22}];
+    collection.insert(data, function(err, result) {
+        if(err)
+        {
+            console.log('Error:'+ err);
+            return;
+        }
+        callback(result);
     });
-    
-var PNoteModel = db.model('PNote',PNoteSchema);
+}
 
-var pNoteEntity = new PNoteModel({path:'\pnote\test',text:'test'});
+MongoClient.connect(DB_CONN_STR, function(err, db) {
+    console.log(err);
+    console.log("连接成功！");
+    insertData(db, function(result) {
+        console.log(result);
+        db.close();
+    });
+});
 
-pNoteEntity.save();
+// //查询
+// var selectData = function(db, callback) {  
+//   //连接到表  
+//   var collection = db.collection('tb2');
+//   //查询数据
+//   var whereStr = {"name":'wilson001'};
+//   collection.find(whereStr).toArray(function(err, result) {
+//     if(err)
+//     {
+//       console.log('Error:'+ err);
+//       return;
+//     }     
+//     callback(result);
+//   });
+// }
+
+// MongoClient.connect(DB_CONN_STR, function(err, db) {
+//   console.log("连接成功！");
+//   selectData(db, function(result) {
+//     console.log(result);
+//     db.close();
+//   });
+// });
+
+// //修改
+// var updateData = function(db, callback) {  
+//     //连接到表  
+//     var collection = db.collection('tb2');
+//     //更新数据
+//     var whereStr = {"name":'wilson001'};
+//     var updateStr = {$set: { "age" : 100 }};
+//     collection.update(whereStr,updateStr, function(err, result) {
+//         if(err)
+//         {
+//             console.log('Error:'+ err);
+//             return;
+//         }     
+//         callback(result);
+//     });
+// }
+
+// MongoClient.connect(DB_CONN_STR, function(err, db) {
+//     console.log("连接成功！");
+//     updateData(db, function(result) {
+//         console.log(result);
+//         db.close();
+//     });
+// });
+
+// //删除
+// var delData = function(db, callback) {  
+//   //连接到表  
+//   var collection = db.collection('tb2');
+//   //删除数据
+//   var whereStr = {"name":'wilson001'};
+//   collection.remove(whereStr, function(err, result) {
+//     if(err)
+//     {
+//       console.log('Error:'+ err);
+//       return;
+//     }     
+//     callback(result);
+//   });
+// }
+
+// MongoClient.connect(DB_CONN_STR, function(err, db) {
+//   console.log("连接成功！");
+//   delData(db, function(result) {
+//     console.log(result);
+//     db.close();
+//   });
+// });
