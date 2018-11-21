587bd2726d609a410b657ca20b630d60a019d1f2
holyond <holyond@163.com>
2018-11-21 22:34:14 +0800

    add1.0

--- site/readme.txt
+++
@@ -1,4 +0,0 @@
-1、部署说明：
-    1.1、npm install
-    1.2、npm install mongodb
-    1.3、安装 mongodb , 并运行
--- site/routes/pnote.js
+++ site/routes/pnote.js
@@ -1,18 +1,28 @@
 var express = require('express');
 var router = express.Router();
-var db=require('./js/mongoHelper');
+var fs = require('fs')
+var pp = require('path')
+// var db=require('./js/mongoHelper');
+
+var storage = require('mini-dbx');
+var db = storage('../../db.json');
+
+
 router.post('/save',function(req,res){
    //获取path
     var path =req.body.path;
     var text = req.body.text;
 
-    //修改数据库
-    var p3={};
-    p3.collection="remark";
-    p3.updateStr={$set: { "text" : text }};
-    p3.whereStr={"path":path};
-    db.update(null,p3,function(result){
-    });
+    //查看有没有
+    var datas = db.select(path)
+    
+    if(datas.length> 0){
+        //修改
+        db.update(path,{data:text},'#==0')
+    }else{
+        //insert
+        db.insert(path,{data : text})
+    }
     res.json({success:path});
 });
 
@@ -21,11 +31,20 @@ router.get('/list',function(req,res,next){
      //获取orginPath
     var path = req.originalUrl;
     //获取文件内容
-    var p2={};
-    p2.collection= 'remark';
-    p2.whereStr={"text":{$ne:''}};
-    db.select(null,p2,function (result) {
-        res.render('pnote/list', { list : result });
+    // var p2={};
+    // p2.collection= 'remark';
+    // p2.whereStr={"text":{$ne:''}};
+    // db.select(null,p2,function (result) {
+    //     res.render('pnote/list', { list : result });
+    // });
+
+    fs.readFile( pp.resolve(__dirname, '../..')+  '/db.json','utf8',function (err, data) {
+        if(err) console.log(err);
+        var test1=JSON.parse(data);
+        var rs = new Array()
+        for(var key in test1)
+            rs.push({ path : key})
+        res.render('pnote/list', { list : rs });
     });
 
 });
@@ -34,25 +53,16 @@ router.get('/*', function(req, res, next) {
     //获取orginPath
     var path = req.originalUrl;
     //获取文件内容
-    var p2={};
-    p2.collection= 'remark';
-    p2.whereStr={"path":path};
-    db.select(null,p2,function (result) {
-        var txt = '';
-        if(result.length >0)
-        {
-            txt=result[0].text;
-        }
-        else
-        {
-            //插入一条空数据
-            var p1={};
-            p1.collection='remark';
-            p1.data= {"path":path,"text":""};
-            db.insert(null,p1,function (result) {});
-        }
-        res.render('pnote/index', { text: txt,path: path });
-    });
+    var result = db.select(path,'#==0')
+    if(result.length >0)
+    {
+        txt=result[0].data;
+    }
+    else
+    {
+       txt =''
+    }
+    res.render('pnote/index', { text: txt,path: path });
 
 });
 
